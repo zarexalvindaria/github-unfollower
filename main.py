3fcab76d3005fb4b83d424180ff18f815df9f2a0
zarexalvindaria <zarexalvindaria@users.noreply.github.com>
2022-01-07 07:58:47 +0800

    feat: Enable multi-page user lists support
    Include followers and following exceeding the 100 users max per page
    limit in GitHub API.

--- main.py
+++ main.py
@@ -4,28 +4,60 @@ import json
 base_url = 'https://api.github.com/users/'
 
 
-def get_user_list(url):
-    with urllib.request.urlopen(url) as response:
-        users = json.loads(response.read().decode())
-        user_list = []
-        for user in users:
-            user_list += [user['login']]
-        return user_list
+# def get_user_list(*args, url):
+#     with urllib.request.urlopen(url) as response:
+#         users = json.loads(response.read().decode())
+#         for user in users:
+#             user_list += [user['login']]
+#         return user_list
 
 
 # Get list of followers
 def get_followers(userid):
-    return get_user_list(base_url + userid + '/followers?per_page=100&page=1')
+    i = 1
+    user_list = []
+
+    while True:
+        try:
+            url = base_url + userid + '/followers?per_page=100&page=' + str(i)
+            with urllib.request.urlopen(url) as response:
+                users = json.loads(response.read().decode())
+                if users:
+                    for user in users:
+                        user_list += [user['login']]
+                elif not users:
+                    break
+        except:
+            break
+        i += 1
+    return user_list
 
 
 # Get list of following
 def get_following(userid):
-    return get_user_list(base_url + userid + '/following?per_page=100&page=1')
+    i = 1
+    user_list = []
+
+    while True:
+        try:
+            url = base_url + userid + '/following?per_page=100&page=' + str(i)
+            with urllib.request.urlopen(url) as response:
+                users = json.loads(response.read().decode())
+                if users:
+                    for user in users:
+                        user_list += [user['login']]
+                elif not users:
+                    break
+        except:
+            break
+        i += 1
+    return user_list
 
 
 if __name__ == '__main__':
     username = str(input("Enter your GitHub username: "))
     followers = get_followers(username)
+
     following = get_following(username)
     no_follow_back_users = list(set(following) - set(followers))
     fans = list(set(followers) - set(following))
@@ -42,14 +74,12 @@ if __name__ == '__main__':
         following_count += 1
     print(f'following:', following_count)
 
-
-
     # Print no follow back users
     print(f'\nHere are the users not following you back:')
     for no_follow_back in no_follow_back_users:
         print(no_follow_back)
 
     # Print fans
-    print(f'\nHere are your fans:')
+    print(f'\n\nHere are your fans:')
     for fan in fans:
         print(fan)
